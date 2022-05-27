cdba6feabcf98a86f7b1b4c6e9d0e2ab4409a771
Joshua Egbuonu <joshuaegbuonu@gmail.com>
2022-05-27 14:00:10 +0100

    first commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,781 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="top">
+    <nav>
+      <img src="@/assets/robin.png" alt="logo" id="robin">
+      <router-link to="/" id="home">Pricing</router-link> 
+      <router-link to="/" id="home">Docs</router-link>
+      <router-link to="/" id="home">Use Cases</router-link>
+      <router-link to="/" id="home">SDKs</router-link>
+      <router-link to="/" id="home">Enterprise</router-link>
+      <router-link to="/" id="home">Try Demo</router-link>
+      <router-link to="/" id="home">Sign In</router-link> 
+      <router-link to="/" id="home">Get Started</router-link>
+    </nav>
+  </div>
+  <div class="firstdiv">
+    <h1 id="simple">Simple & Transparent Pricing. No surprises.</h1>
+    <h3 id='the'>The only tool you will need to power conversations. </h3>
+    <button id="btn1">
+    Start Free Trial
+    <img src="@/assets/Arrow.png" alt="arrow image" id="ticker">
+    </button>
+  </div>
+  <div class="boxes">
+    <div id="box1">
+        <p class="maroon">Starter</p>
+        <p class="dollars">$69/mo</p>
+        <hr class="single">
+        <div class="features">
+          <img src="@/assets/circlecheck.png" alt="arrow image" class="circle">
+          <p class="core">Core Chat Features</p>
+        </div>
+        <div class="features1">
+          <img src="@/assets/circlecheck.png" alt="arrow image" class="circle1">
+          <p class="core1"> <span class="opaque">10,000 </span>Monthly Active Users</p>
+        </div>
+        <div class="features2">
+          <img src="@/assets/circlecheck.png" alt="arrow image" class="circle2">
+          <p class="core2">MAU Overage Rate</p>
+        </div>
+        <div class="features3">
+          <img src="@/assets/circlecheck.png" alt="arrow image" class="circle3">
+          <p class="core3"><span class="opaque">100 </span>Concurrent Included</p>
+        </div>
+        <div class="features4">
+          <img src="@/assets/circlecheck.png" alt="arrow image" class="circle4">
+          <p class="core4">Limited Message Storage</p>
+        </div>
+        <hr class="double">
+        <div class="last">
+          <p class="lasttext">Get Started</p>
+          <img src="@/assets/lastarrow.png" alt="arrow image" class="lastarrow">
+        </div>
+    </div>
+    <div id="box2">
+      <p class="maroon">Growth</p>
+      <p class="dollars">$389/mo</p>
+      <hr class="single">
+        <div class="features">
+          <img src="@/assets/circlecheck.png" alt="arrow image" class="circle">
+          <p class="core">Core Chat Features</p>
+        </div>
+        <div class="features1">
+          <img src="@/assets/circlecheck.png" alt="arrow image" class="circle1">
+          <p class="core1"><span class="opaque">25,000 </span>Monthly Active Users</p>
+        </div>
+        <div class="features2">
+          <img src="@/assets/circlecheck.png" alt="arrow image" class="circle2">
+          <p class="core2">MAU Overage Rate</p>
+          </div>
+        <div class="features3">
+          <img src="@/assets/circlecheck.png" alt="arrow image" class="circle3">
+          <p class="core3"><span class="opaque">100 </span>Concurrent Included</p>
+        </div>
+        <div class="features4">
+            <img src="@/assets/circlecheck.png" alt="arrow image" class="circle4">
+            <p class="core4">Limited Message Storage</p>
+        </div>
+        <hr class="double">
+        <div class="last">
+          <p class="lasttext">Get Started</p>
+          <img src="@/assets/lastarrow.png" alt="arrow image" class="lastarrow">
+        </div>
+    </div>
+    <div id="box3">
+      <p class="maroon">Pro</p>
+      <p class="dollars">$799/mo</p>
+      <hr class="single">
+        <div class="features">
+          <img src="@/assets/circlecheck.png" alt="arrow image" class="circle">
+          <p class="core">Core Chat Features</p>
+        </div>
+        <div class="features1">
+          <img src="@/assets/circlecheck.png" alt="arrow image" class="circle1">
+          <p class="core1"><span class="opaque">50,000 </span>Monthly Active Users</p>
+        </div>
+        <div class="features2">
+          <img src="@/assets/circlecheck.png" alt="arrow image" class="circle2">
+          <p class="core2">MAU Overage Rate</p>
+        </div>
+        <div class="features3">
+          <img src="@/assets/circlecheck.png" alt="arrow image" class="circle3">
+          <p class="core3"><span class="opaque">100 </span>Concurrent Included</p>
+        </div>
+        <div class="features4">
+            <img src="@/assets/circlecheck.png" alt="arrow image" class="circle4">
+            <p class="core4">Limited Message Storage</p>
+        </div>
+        <hr class="double">
+        <div class="last">
+          <p class="lasttext">Get Started</p>
+          <img src="@/assets/lastarrow.png" alt="arrow image" class="lastarrow">
+        </div>
+    </div>
+    <div id="box4">
+      <p class="maroon">Enterprise</p>
+      <p class="dollars">Customized</p>
+      <hr class="single">
+        <div class="features">
+          <img src="@/assets/circlecheck.png" alt="arrow image" class="circle">
+          <p class="core">Core Chat Features</p>
+        </div>
+        <div class="features1">
+          <img src="@/assets/circlecheck.png" alt="arrow image" class="circle1">
+          <p class="core1"><span class="opaque">100,000+ </span>Monthly Active Users</p>
+        </div>
+        <div class="features2">
+          <img src="@/assets/circlecheck.png" alt="arrow image" class="circle2">
+          <p class="core2">MAU Overage Rate</p>
+          </div>
+        <div class="features3">
+          <img src="@/assets/circlecheck.png" alt="arrow image" class="circle3">
+          <p class="core3"><span class="opaque">100 </span>Concurrent Included</p>
+        </div>
+        <div class="features4">
+          <img src="@/assets/circlecheck.png" alt="arrow image" class="circle4">
+          <p class="core4">Limited Message Storage</p>
+        </div>
+        <hr class="double">
+        <div class="last">
+          <p class="lasttext">Get Started</p>
+          <img src="@/assets/lastarrow.png" alt="arrow image" class="lastarrow">
+        </div>
+    </div>
+  </div>
+  <p id="trust">Trusted by experts at</p>
+  <img src="@/assets/screenshot.png" alt="brand logos" class="screenshot">
+  <div class="dropcontainer">
+    <h3 id="common">Commonly asked questions</h3>
+    <p id="ship">Ship in-app chat messaging with all the features your users expect.</p>
+    <div class="dropcontent dropcontent1">
+      <p class="droptext">How is MAU calculated?</p>
+      <img src="@/assets/Mask.png" alt="dropdown arrow" class="mask">
+    </div>
+    <div class="dropcontent">
+      <p class="droptext">What is the difference between Starter, Pro, and Enterprise plans?</p>
+      <img src="@/assets/Mask.png" alt="dropdown arrow" class="mask">
+    </div>
+    <div class="dropcontent">
+      <p class="droptext">What happens if I hit my MAU limit?</p>
+      <img src="@/assets/Mask.png" alt="dropdown arrow" class="mask">
+    </div>
+    <div class="dropcontent">
+      <p class="droptext">How do I purchase higher levels of support?</p>
+      <img src="@/assets/Mask.png" alt="dropdown arrow" class="mask">
+    </div>
+    <div class="dropcontent">
+      <p class="droptext">What other limits do I need to be aware of?</p>
+      <img src="@/assets/Mask.png" alt="dropdown arrow" class="mask">
+    </div>
+    <div class="dropcontent">
+      <p class="droptext">Is every feature automatically included in the enterprise plan?</p>
+      <img src="@/assets/Mask.png" alt="dropdown arrow" class="mask">
+    </div>
+    <div class="dropcontent">
+      <p class="droptext">Do the prices listed on the pricing page include tax?</p>
+      <img src="@/assets/Mask.png" alt="dropdown arrow" class="mask">
+    </div>
+    <p id="got">Got additional questions?</p>
+    <p id="we">We would love to have a chat with you.</p>
+    <div id="reach">
+      <p id="contact">Contact Support</p>
+      <img src="@/assets/bluearrow.png" alt="arrow" id="bluearrow">
+    </div>
+  </div>
+  <div class="chatbox">
+    <div class="chatters">
+      <p id="chattext">CHAT</p>
+      <h2 id="see">See the all-in-one chat system in action</h2>
+      <p id="only">The only tool you will need to power conversations and interactions on your product. <span id="go">Go live within days.</span></p>
+      <div class="startbtn">
+        <p id="start">Start Demo</p>
+        <img src="@/assets/blackarrow.png" alt="arrow" id="blackarrow">
+      </div>
+    </div>
+    <img src="@/assets/chats.png" alt="arrow" id="chats">
+  </div>
+  <div class="bluebox">
+    <h2 id="ready">Ready to have a chat?</h2>
+    <p id="love">We'd love to talk about your messaging strategy and help make your software more conversational.</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
   }
 }
 </script>
+
+<style scoped>
+.top{
+position: fixed;
+width: 100%;
+height: 80px;
+left: 0px;
+top: 0px;
+background: #FFFFFF;
+box-shadow: 0px 10px 20px rgba(0, 104, 255, 0.09);
+display: inline-block;
+z-index: 1000;
+}
+
+.top router-link-active{
+
+}
+
+#robin{
+padding-left: px;
+margin-top: 16px;
+}
+
+.firstdiv{
+text-align: center;
+}
+
+#simple{
+width: 100%;
+max-width: 720px;
+margin: 228px auto 28px;
+font-family: 'Zona Pro';
+font-style: normal;
+font-weight: 700;
+font-size: 50px;
+line-height: 54px;
+text-align: center;
+color: #000000;
+}
+
+#the{
+width: 100%;
+height: 25px;
+font-family: 'Google Sans';
+font-style: normal;
+font-weight: 400;
+font-size: 20px;
+line-height: 25px;
+text-align: center;
+color: #000000;
+opacity: 0.8;
+}
+
+#btn1{
+width: 207px;
+height: 50px;
+background: #4568D1;
+border-radius: 30.5px;
+margin: 32px auto 91px auto;
+font-family: 'Google Sans';
+font-style: normal;
+font-weight: 700;
+font-size: 16px;
+line-height: 20px;
+text-align: center;
+color: #FFFFFF;
+}
+
+#ticker{
+padding-left: 44px;
+}
+
+.boxes{
+margin: auto;
+width: 100%;
+max-width: 1294px;
+display: flex;
+flex-direction: row;
+justify-content: space-around;
+}
+
+#box1{
+box-sizing: border-box;
+width: 310px;
+height: 499px;
+background: #F5F7FC;
+border: 1px solid #4568D1;
+border-radius: 25px;
+}
+
+#box2{
+box-sizing: border-box;
+width: 310px;
+height: 499px;
+background: #F5F7FC;
+border: 1px solid #4568D1;
+border-radius: 25px;
+margin-left: 18px;
+}
+
+#box3{
+box-sizing: border-box;
+width: 310px;
+height: 499px;
+background: #F5F7FC;
+border: 1px solid #4568D1;
+border-radius: 25px;
+margin-left: 18px;
+}
+
+#box4{
+box-sizing: border-box;
+width: 310px;
+height: 499px;
+background: #F5F7FC;
+border: 1px solid #4568D1;
+border-radius: 25px;
+margin-left: 18px;
+}
+
+.maroon{
+font-family: 'Zona Pro';
+font-style: normal;
+font-weight: 700;
+font-size: 18px;
+line-height: 19px;
+color: #EA8D51;
+padding-left: 28px;
+display: inline-block;
+margin-block-start: 0;
+margin-block-end: 0;
+margin-top: 36px;
+}
+
+.dollars{
+font-family: 'Google Sans';
+font-style: normal;
+font-weight: 700;
+font-size: 35px;
+color: #000000;
+line-height: 45px;
+padding-left: 28px;
+margin-block-start: 0;
+margin-block-end: 0;
+margin-top: 8px;
+}
+
+hr.single{
+width: 257px;
+height: 0px;
+opacity: 0.2;
+border: 1px solid #BBC1D6;
+margin-block-start: 0;
+margin-block-end: 0;
+margin-top: 16px;
+}
+
+.circle{
+width: 16px;
+height: 16px;
+padding: 0;
+}
+
+.circle1{
+width: 16px;
+height: 16px;
+}
+
+.circle2{
+width: 16px;
+height: 16px;
+}
+
+.circle3{
+width: 16px;
+height: 16px;
+}
+
+.circle4{
+width: 16px;
+height: 16px;
+}
+
+.core{
+font-family: 'Google Sans';
+font-style: normal;
+font-weight: 400;
+font-size: 16px;
+line-height: 20px;
+color: #485476;
+opacity: 0.8;
+padding-left: 10.67px;
+}
+
+.core1{
+font-family: 'Google Sans';
+font-style: normal;
+font-weight: 400;
+font-size: 16px;
+line-height: 20px;
+color: #485476;
+opacity: 0.8;
+padding-left: 10.67px;
+}
+
+.core2{
+font-family: 'Google Sans';
+font-style: normal;
+font-weight: 400;
+font-size: 16px;
+line-height: 20px;
+color: #485476;
+opacity: 0.8;
+padding-left: 10.67px;
+}
+
+.core3{
+font-family: 'Google Sans';
+font-style: normal;
+font-weight: 400;
+font-size: 16px;
+line-height: 20px;
+color: #485476;
+opacity: 0.8;
+padding-left: 10.67px;
+}
+
+.core4{
+font-family: 'Google Sans';
+font-style: normal;
+font-weight: 400;
+font-size: 16px;
+line-height: 20px;
+color: #485476;
+opacity: 0.8;
+padding-left: 10.67px;
+}
+
+.features{
+margin-top: 18px;
+margin-left: 28px;
+display: flex;
+flex-direction: row;
+justify-content: flex-start;
+align-items: center;
+width: auto;
+height: 20px;
+}
+
+.features1{
+margin-top: 30px;
+margin-left: 28px;
+display: flex;
+flex-direction: row;
+justify-content: flex-start;
+align-items: center;
+width: auto;
+height: 20px;
+}
+
+.features2{
+margin-top: 30px;
+margin-left: 28px;
+display: flex;
+flex-direction: row;
+justify-content: flex-start;
+align-items: center;
+width: auto;
+height: 20px;
+}
+
+.features3{
+margin-top: 30px;
+margin-left: 28px;
+display: flex;
+flex-direction: row;
+justify-content: flex-start;
+align-items: center;
+width: auto;
+height: 20px;
+}
+
+.features4{
+margin-top: 30px;
+margin-left: 28px;
+display: flex;
+flex-direction: row;
+justify-content: flex-start;
+align-items: center;
+width: auto;
+height: 20px;
+}
+
+.last{
+display: flex;
+flex-direction: row;
+justify-content: flex-start;
+align-items: center;
+margin-left: 28px;
+margin-top: 27px;
+}
+
+.lasttext{
+font-family: 'Google Sans';
+font-style: normal;
+font-weight: 700;
+font-size: 16px;
+line-height: 20px;
+color: #4568D1;
+margin-block-start: 0;
+margin-block-end: 0;
+}
+
+.lastarrow{
+width: 10px;
+margin-left: 6px;
+}
+
+hr.double{
+margin-block-start: 0;
+margin-block-end: 0;
+margin-top: 52px;
+width: 257px;
+height: 0px;
+opacity: 0.2;
+border: 1px solid #BBC1D6;
+}
+
+.opaque{
+opacity: 100%;
+font-weight: 700;
+}
+
+#trust{
+font-family: 'Zona Pro';
+font-style: normal;
+font-weight: 700;
+font-size: 15px;
+line-height: 18px;
+text-align: center;
+text-transform: uppercase;
+color: #000000;
+opacity: 0.4;
+margin-top: 120px;
+}
+
+.screenshot{
+width: 1011px;
+max-width: 100%;
+height: 103px;
+margin: 19px auto 0;
+text-align: center;
+display: block;
+}
+
+.dropcontainer{
+width: 1238px;
+max-width: 100%;
+height: 1366px;
+background: #F5F7FC;
+border-radius: 25px;
+margin: 95px auto;
+}
+
+#common{
+font-family: 'Zona Pro';
+font-style: normal;
+font-weight: 700;
+font-size: 35px;
+line-height: 41px;
+text-align: center;
+color: #000000;
+text-align: center;
+margin-block-start: 0;
+margin-block-end: 0;
+padding-top: 71px;
+}
+
+#ship{
+width: 362px;
+height: 60px;
+font-family: 'Google Sans';
+font-style: normal;
+font-weight: 400;
+font-size: 19px;
+line-height: 30px;
+text-align: center;
+color: #000000;
+opacity: 0.8;
+text-align: center;
+margin-block-start: 0;
+margin-block-end: 0;
+margin: 23px auto;
+}
+
+.droptext{
+font-family: 'Zona Pro';
+font-style: normal;
+font-weight: 700;
+font-size: 20px;
+line-height: 22px;
+color: #000000;
+padding: 27px 0 37px 50px;
+margin-block-start: 0;
+margin-block-end: 0;
+}
+
+.dropcontent{
+width: 879px;
+height: 86px;
+background: #FFFFFF;
+margin: 20px auto;
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+}
+
+.dropcontent1{
+margin: 35px auto;
+}
+
+.mask{
+height: 8px;
+width: 14px;
+border-radius: 0px;
+margin: 39px 39px 39px;
+}
+
+#got{
+font-family: 'Zona Pro';
+font-style: normal;
+font-weight: 700;
+font-size: 17px;
+line-height: 20px;
+text-align: center;
+color: #000000;
+margin-top: 68px;
+}
+
+#we{
+font-family: 'Google Sans';
+font-style: normal;
+font-weight: 400;
+font-size: 16px;
+line-height: 30px;
+color: #000000;
+opacity: 0.8;
+margin-top: 8px;
+text-align: center;
+}
+
+#reach{
+box-sizing: border-box;
+width: 207px;
+height: 50px;
+background: #FFFFFF;
+border-radius: 30.5px;
+display: flex;
+flex-direction: row;
+align-items: center;
+margin: 20px auto;
+}
+
+#contact{
+font-family: 'Google Sans';
+font-style: normal;
+font-weight: 700;
+font-size: 16px;
+line-height: 20px;
+color: #4568D1;
+padding-left: 26px;
+padding-right: 23px;
+}
+
+#bluearrow{
+width: 10px;
+}
+
+.chatbox{
+width: 100%;
+height: 568px;
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+}
+
+.chatters{
+display: flex;
+flex-direction: column;
+}
+
+#chattext{
+font-family: 'Zona Pro';
+font-style: normal;
+font-weight: 700;
+font-size: 15px;
+line-height: 18px;
+color: #000000;
+opacity: 0.4;
+margin-block-start: 0;
+margin-block-end: 0;
+margin: 73px auto 0 217px;
+}
+
+#see{
+font-family: 'Zona Pro';
+font-style: normal;
+font-weight: 700;
+font-size: 35px;
+line-height: 41px;
+color: #000000;
+margin-block-start: 0;
+margin-block-end: 0;
+margin: 22px auto 0 217px;
+max-width: 329px;
+}
+
+#only{
+margin-block-start: 0;
+margin-block-end: 0;
+font-family: 'Google Sans';
+font-style: normal;
+font-weight: 400;
+font-size: 19px;
+line-height: 30px;
+color: #000000;
+opacity: 0.8;
+margin: 37px auto 0 217px;
+max-width: 342px;
+}
+
+#go{
+font-weight: 700;
+}
+
+.startbtn{
+width: 207px;
+height: 50px;
+border: 1px solid #000000;
+border-radius: 30.5px;
+margin-block-start: 0;
+margin-block-end: 0;
+margin: 82px auto auto 217px;
+display: flex;
+flex-direction: row;
+align-items: center;
+}
+
+#start{
+font-family: 'Google Sans';
+font-style: normal;
+font-weight: 700;
+font-size: 16px;
+line-height: 20px;
+color: #333333;
+padding-left: 36px;
+}
+
+#blackarrow{
+width: 10px;
+height: 8px;
+margin-left: 52px;
+}
+
+.bluebox{
+width: 100%;
+height: 718px;
+background: #0C1F55;
+}
+</style>
